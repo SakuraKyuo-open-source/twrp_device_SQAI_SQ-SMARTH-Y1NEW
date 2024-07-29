c3a882f741a33e83021f8b93a45ba31c2cdc3642
SakuraKyuo <sakurakyuo@protonmail.com>
2024-07-30 00:06:10 +0800

    Y1NEW: Bringup for SQAI Y1NEW

--- device.mk
+++ device.mk
@@ -14,52 +14,22 @@
 # limitations under the License.
 #
 
-# Virtual A/B
-ENABLE_VIRTUAL_AB := true
-
 # Inherit product
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Device
-LOCAL_PATH := device/xiaomi/viva
+LOCAL_PATH := device/SQ/Y1NEW
 
 PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_TARGET_VNDK_VERSION := 31
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# A/B
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
-    
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    system_ext \
-    product \
-    vendor \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor
-    
 # Boot Control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service \
     android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-mtkimpl.recovery
-    
+
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
@@ -76,7 +46,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd \
     android.hardware.fastboot@1.0-impl-mock
-    
+
 # Additional Libs
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
@@ -87,7 +57,7 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-    
+
 # USB
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.sys.usb.storage.type=mtp
