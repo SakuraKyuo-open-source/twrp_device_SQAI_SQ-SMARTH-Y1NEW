231a11933fc6305668cb3cb79a87ad58e4e652de
SakuraKyuo <sakurakyuo@protonmail.com>
2024-07-30 00:38:36 +0800

    Y1NEW: Bringup Binary files for Y1NEW

--- device.mk
+++ device.mk
@@ -50,12 +50,10 @@ PRODUCT_PACKAGES += \
 # Additional Libs
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
-    libkeymaster41 \
     libpuresoftkeymasterdevice
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # USB
--- recovery/root/system/bin/symlink.sh
+++ recovery/root/system/bin/symlink.sh
@@ -1,5 +1,4 @@
 #!/system/bin/sh
 mkdir -p /dev/block/bootdevice/by-name/;
-ln -s /dev/block/by-name/boot_a /dev/block/bootdevice/by-name/boot_a;
-ln -s /dev/block/by-name/boot_b /dev/block/bootdevice/by-name/boot_b;
+ln -s /dev/block/by-name/boot /dev/block/bootdevice/by-name/boot;
 exit 0;
